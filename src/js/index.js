1e042bdd3d2fe5c58f79ecc1763e74f3650b6492
Leonardo José Alves Gouvea <leonardo.j.gouvea@outlook.com>
2026-02-02 22:30:45 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,82 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Objetivo:
+  // Enviar um texto de um formulário para uma API do n8n e exibir o resultado o código html, css e colocar a animação no fundo da tela do site.
+
+  // Passos:
+  // 1. No JavaScript, pegar o evento de submit do formulário para evitar o recarregamento da página.
+  const form = document.querySelector(".form-group");
+  const inputDesc = document.getElementById("description");
+  const codigoHtml = document.getElementById("html-code");
+  const codigoCss = document.getElementById("css-code");
+  const previewSection = document.getElementById("preview-section");
+
+  form.addEventListener("submit", async function (event) {
+    event.preventDefault(); // Evita o recarregamento da página
+
+    // 2. Obter o valor digitado pelo usuário no campo de texto.
+    const desc = inputDesc.value.trim();
+
+    if (!desc) {
+      return;
+    }
+
+    // 3. Exibir um indicador de carregamento enquanto a requisição está sendo processada.
+    showLoading(true);
+
+    // 4. Fazer uma requisição HTTP (POST) para a API do n8n, enviando o texto do formulário no corpo da requisição em formato JSON.
+    try {
+      const answer = await fetch(
+        "https://leonardogouvea.app.n8n.cloud/webhook/fundo-magico",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ desc }),
+        },
+      );
+
+      const data = await answer.json();
+      codigoHtml.textContent = data.html || "";
+      codigoCss.textContent = data.css || "";
+
+      previewSection.style.display = "block";
+      previewSection.innerHTML = data.html;
+
+      let styleTag = document.getElementById("dynamic-style");
+      //se essa tag já existir, removemos antes de criar uma nova
+      if (styleTag) {
+        styleTag.remove();
+      }
+
+      if (data.css) {
+        styleTag = document.createElement("style");
+        styleTag.id = "dynamic-style";
+        styleTag.textContent = data.css;
+        document.head.appendChild(styleTag);
+      }
+    } catch (error) {
+      console.error("Erro ao enviar a requisição:", error);
+      codigoHtml.textContent = "Não consegui gerar o HTML, tente novamente.";
+      codigoCss.textContent = "Não consegui gerar o CSS, tente novamente.";
+      previewSection.innerHTML = "";
+    } finally {
+      showLoading(false);
+    }
+  });
+
+  function showLoading(isLoading) {
+    const botaoEnviar = document.getElementById("generate-btn");
+    if (isLoading) {
+      botaoEnviar.textContent = "Carregando Background...";
+    } else {
+      botaoEnviar.textContent = "Gerar Fundo Mágico";
+    }
+  }
+
+  // 5. Receber a resposta da API do n8n (esperando um JSON com o código HTML/CSS do background).
+  // 6. Se a resposta for válida, exibir o código HTML/CSS retornado na tela:
+  //    - Mostrar o HTML e CSS gerado em uma área de preview.
+  //    - Inserir o CSS retornado dinamicamente na página para aplicar o background.
+  // 7. Remover o indicador de carregamento após o recebimento da resposta.
+});
